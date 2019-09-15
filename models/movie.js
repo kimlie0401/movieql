e648a6b057f617cfa13bb42a8d560a5427e90809
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-09-14 18:08:56 -0700

    Updated

--- models/movie.js
+++ models/movie.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
+  id: {
+    type: Number
+  },
   name: {
     type: String,
     required: true
   },
   score: {
+    type: Number
+  },
+  year: {
     type: Number
   }
 });
